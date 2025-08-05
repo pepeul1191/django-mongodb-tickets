f3188e06473ad10d81f7c94a2a0e5896c11d5119
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2025-08-05 11:31:35 -0500

    creacion de modulo de managment

---
+++ main/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def index(request):
+  context = {
+    'page_title': 'Página Principal',
+    'items': ['Item 1', 'Item 2', 'Item 3']
+  }
+  return render(request, 'index.html', context)
